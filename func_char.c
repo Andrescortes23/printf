1d6eaa977577148b08f46cff3157e76799755c4f
Andres Cortes <1805@holbertonschool.com>
2020-07-20 03:01:19 +0000

    Created a function to print characters

---
+++ func_char.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *func_char - function to print char
+ *@ap: list of arguments
+ *Return: number of arguments
+ */
+int func_char(va_list ap)
+{
+	_putchar(va_arg(ap, int));
+	return (1);
+}
