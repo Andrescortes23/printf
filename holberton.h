75227e9ae780fe0d4eccdf004c502598d7ea8ac6
Andres Cortes <1805@holbertonschool.com>
2020-07-21 00:28:38 +0000

    Correcting issues in struct and adding commnets for Betty

--- holberton.h
+++ holberton.h
@@ -7,17 +7,16 @@
 #include <stdlib.h>
 
 /**
- * type_op - struct type_op
+ * struct type_op - struc to use
  * @op: the operator
- * @f: function pointer
- *
+ * @func: function pointer
  * Description: contain char pointer and function pointer
  */
 typedef struct type_op
 {
 	char *op;
 	int (*func)(va_list);
-}type_t;
+} type_t;
 int _putchar(char c);
 void _puts(char *str);
 int _printf(const char *format, ...);
